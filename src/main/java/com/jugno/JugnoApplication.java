bf05ddf536dff259cf45a864be8d9deb1b04c68e
Muhammad Jawad Butt <m_jawad_butt@yahoo.com>
2022-04-19 22:00:59 +0500

    Switching to maven from gradle and java/spring from kotlin.

---
+++ src/main/java/com/jugno/JugnoApplication.java
@@ -0,0 +1,40 @@
+package com.jugno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+//@PropertySources({
+//        @PropertySource("classpath:rdbms.${.properties"),
+//        @PropertySource("classpath:bar.properties")
+//})
+public class JugnoApplication {
+
+  public JugnoApplication() {
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(com.jugno.JugnoApplication.class, args);
+  }
+
+  @EventListener(ApplicationReadyEvent.class)
+  // See: https://stacktraceguru.com/springboot/run-method-on-startup
+  public void runAfterStartup() {
+    System.out.println("Yaaah, I am running........");
+  }
+
+//  @Bean
+//  public PhysicalNamingStrategy getPropertyNameBoundNamingStrategy() {
+//    return new CamelCaseToUnderscoresNamingStrategy() {
+//
+//      @Override
+//      public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+//        return new Identifier(identifier.getText(), true);
+//      }
+//
+//    };
+//  }
+
+}
---
+++ src/test/java/com/jugno/JugnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jugno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JugnoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
